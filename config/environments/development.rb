5d03141bf6e7b781b3987b9a779898e672315d1c
nazip <nazipm@mail.ru>
2015-09-28 17:06:28 +0300

    lesson-20 done

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,8 +39,17 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.preview_path = "#{Rails.root}/views"
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.delivery_method = :letter_opener
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "emailforwebapplications@gmail.com",
+      :password             => "OnlyForWebApplications",
+      :authentication       => :login,
+      :enable_starttls_auto => true
+  }
 end
